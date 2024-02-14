170142d1ec6507b5874386751e6814ebb49848ce
Vova iz Tambova <voller.ruden@yandex.ru>
2024-02-14 12:37:43 +0300

    use api word

--- src/App.vue
+++ src/App.vue
@@ -6,29 +6,51 @@ import GameWord from './components/GameWord.vue'
 import GamePopup from './components/GamePopup.vue'
 import GameNotification from './components/GameNotification.vue'
 import { computed, ref, watch } from 'vue'
+import axios from 'axios'
+import { workerData } from 'worker_threads'
 
-const word = ref('василий')
+const word = ref('')
+const getRandomWord = async () => {
+  try {
+    const { data } = await axios<{ FirstName: string }>(
+      'https://api.randomdatatools.ru/?unescaped=false&params=FirstName'
+    )
+    word.value = data.FirstName.toLowerCase()
+  } catch (err) {
+    console.log(err)
+    word.value = ''
+  }
+}
+
+getRandomWord()
+
 const letters = ref<string[]>([])
 
 const correctLetters = computed(() => letters.value.filter((x) => word.value.includes(x)))
 const wrongLetters = computed(() => letters.value.filter((x) => !word.value.includes(x)))
+const isLose = computed(() => wrongLetters.value.length == 6)
+const isWin = computed(() => [...word.value].every((x) => correctLetters.value.includes(x)))
 
 const notification = ref<InstanceType<typeof GameNotification> | null>(null)
 const popup = ref<InstanceType<typeof GamePopup> | null>(null)
 
 watch(wrongLetters, () => {
-  if (wrongLetters.value.length == 6) {
+  if (isLose.value) {
     popup.value?.open('loose')
   }
 })
 
 watch(correctLetters, () => {
-  if ([...word.value].every((x) => correctLetters.value.includes(x))) {
+  if (isWin.value) {
     popup.value?.open('win')
   }
 })
 
 window.addEventListener('keydown', ({ key }) => {
+  if (isLose.value || isWin.value) {
+    return
+  }
+
   if (letters.value.includes(key)) {
     notification.value?.open()
     setTimeout(() => notification.value?.close(), 2000)
@@ -39,7 +61,8 @@ window.addEventListener('keydown', ({ key }) => {
   }
 })
 
-const restart = () => {
+const restart = async () => {
+  await getRandomWord()
   letters.value = []
   popup.value?.close()
 }
