189c3db2129c7add9fb66a6b988e03415762933f
Vova iz Tambova <voller.ruden@yandex.ru>
2024-02-14 10:01:16 +0300

    add word+wrong letters

--- src/App.vue
+++ src/App.vue
@@ -5,14 +5,32 @@ import GameWrongLetters from './components/GameWrongLetters.vue'
 import GameWord from './components/GameWord.vue'
 import GamePopup from './components/GamePopup.vue'
 import GameNotification from './components/GameNotification.vue'
+import { computed, ref } from 'vue'
+
+const word = ref('василий')
+const letters = ref<string[]>([])
+const correctLetters = computed(() => letters.value.filter((x) => word.value.includes(x)))
+const wrongLetters = computed(() => letters.value.filter((x) => !word.value.includes(x)))
+
+window.addEventListener('keydown', ({ key }) => {
+  console.log('raw key', key)
+  if (/[а-яА-ЯёЁ]/.test(key)) {
+    console.log('filtered key', key)
+    letters.value.push(key.toLowerCase())
+  }
+})
 </script>
 
 <template>
+  {{ word }}
+  {{ letters }}
+  {{ correctLetters }}
+  {{ wrongLetters }}
   <GameHeader />
   <div class="game-container">
     <GameFigure />
-    <GameWrongLetters />
-    <GameWord />
+    <GameWrongLetters :wrong-letters="wrongLetters" />
+    <GameWord :word="word" :correct-letters="correctLetters" />
   </div>
 
   <GamePopup v-if="false" />
